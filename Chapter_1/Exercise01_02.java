50f0043c65099bdd1a09e206baca709170a2d548
Mr. Young <masyoung@hawaii.edu>
2020-07-20 23:59:30 -1000

    Did Exercises 1.1.1 1.1.2 and 1.1.4

---
+++ Chapter_1/Exercise01_02.java
@@ -0,0 +1,15 @@
+/**	Exercise 1.1.2
+		Write javadoc comments for the given method.
+  */
+public class Exercise01_02 {
+	/**	Checks if given Person has the same name is this Person.
+			@param per The Person whose name will be checked against this Person
+			@return Zero if the names are the same, not zero if the names are not the same
+		*/
+	public int compareTo(Person per) {
+		if (familyName.compareTo(per.familyName) == 0)
+			return givenName.compareTo(per.givenName);
+		else
+			return familyName.compareTo(per.familyName);
+	}
+}
---
+++ Chapter_1/Resizable.java
@@ -0,0 +1,8 @@
+/** Interface to resize an object 
+		Exercise 1.1.1
+  */
+public interface Resizable {
+	/**	Resizes an object
+	  */
+		void resize();
+}
