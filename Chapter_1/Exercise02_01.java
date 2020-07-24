ea3d475051881de5554c7be850049800cfeb229b
Mr. Young <mr.young@metatheory.org>
2020-07-24 04:46:29 -1000

    Did exercises 1.2.1 and 1.2.2

---
+++ Chapter_1/Exercise02_01.java
@@ -0,0 +1,48 @@
+/** Accessor and modifier methods fo class Computer. 
+		Exercise 1.2.1
+	*/
+public class Exercise02_01 {
+
+	// accessor methods
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public String getProcessor() {
+		return processor;
+	}
+
+	public double getRamSize() {
+		return ramSize;
+	}
+
+	public int getDiskSize() {
+		return diskSize;
+	}
+
+	private double getProcessorSpeed() {
+		return processorSpeed;
+	}
+
+
+	// modifier methods
+	public String setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String setProcessor(String processor) {
+		this.processor = processor;
+	}
+
+	public double setRamSize(double ramSize) {
+		this.ramSize = ramSize;
+	}
+
+	public int setDiskSize(int diskSize) {
+		this.diskSize = diskSize;
+	}
+
+	public double setProcessorSpeed(double processorSpeed) {
+		this.processorSpeed = processorSpeed;
+	}
+}
