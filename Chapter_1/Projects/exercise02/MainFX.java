752ddd82e43243f0c95dac16baa9ef0e5b7140e2
Mr. Young <masyoung@hawaii.edu>
2020-08-10 18:24:36 -1000

    Completed project 1.2

--- Chapter_1/Projects/exercise02/MainFX.java
+++ Chapter_1/Projects/exercise02/MainFX.java
@@ -17,21 +17,36 @@ import java.util.Scanner;
 
 /** Collects information on Students and Employees. */
 public class MainFX extends Application {
+/** Array of Person objects. */
 private Person[] people;
+/** The pane that holds all nodes. */
 private BorderPane pane;
+/** The pane for holding nodes related to Student objects. */
 private StudentPane sPane;
+/**  The pane for holding nodes related to Employee ojects */
 private EmployeePane ePane;
+/** Array of Button obects. */
 private Button[] btNext;
+/** The text field for the first question. */
 private TextField tfStudentOrEmployee;
+/** Text object for the word "Record". */
 private Text tRecord;
+/** Text object for the word "Record (continued)". */
 private Text tContinue;
+/** Text object for the word "Student". */
 private Text tStudent;
+/** Text object for the word "Employee". */
 private Text tEmployee;
+/** Text object for a blank word. */
 private Text blank;
+/** Reference variable for a given people[i]. */
 private Person individual;
+/** Variable to determine if an object is a Student. */
 private boolean isStudent;
+/** The index of the people array. */
 private int index;
 
+  /** Initialize/declare some variables. */
   public MainFX() {
    pane = new BorderPane();
    sPane = new StudentPane();
@@ -56,51 +71,71 @@ private int index;
   /** Overrides the start method from the Application class. */
   @Override
   public void start(Stage primaryStage) {
+    // Prompt user to enter the number of records to be added
     Scanner input = new Scanner(System.in);
     System.out.print("\nHow many records will be entered?: ");
+
+    // Initialize the people array
     int numberOfRecords = input.nextInt();
     people = new Person[numberOfRecords];
-
+    
+    // Set padding for the pane
     pane.setPadding(new Insets(8, 5, 5, 5));
-    setHeader(tRecord, blank, new Text(""));
-    setButton(0);
 
-    Scene scene = new Scene(pane, 400, 150);
+    // Create a scene and add it to the stage
+    Scene scene = new Scene(pane);
     primaryStage.setTitle("Persons of Interest");
     primaryStage.setScene(scene);
     primaryStage.show();
       
-    tRecord.setText(String.format("Record %d", index + 1));
-    tContinue.setText(String.format("Record %d (continued)", index + 1));
-    firstEntry();
+    // Set up the first page of the current record
+    firstEntry(primaryStage);
 
-    btNext[0].setOnAction(e -> {        
-     boolean student = tfStudentOrEmployee.getText().equalsIgnoreCase("S");
-     boolean employee = tfStudentOrEmployee.getText().equalsIgnoreCase("E");
-     isStudentObject(student);
 
-     if (student) { 
-       declarePerson();
-       setHeader(tContinue, tStudent, blank);
-       pane.setCenter(sPane);
-       primaryStage.setWidth(575);
-       primaryStage.setHeight(310);
-       setButton(1);
-     } else if (employee) {
-       declarePerson();
-       setHeader(tContinue, tEmployee, blank);
-       pane.setCenter(ePane);
-       ePane.page1();
-       primaryStage.setWidth(510);
-       primaryStage.setHeight(270);
-       setButton(2);
+    // Assign actions to the btNext[0]
+    btNext[0].setOnAction(e -> {
+
+      // Determine if current record should be Student or Employee object
+      boolean student = tfStudentOrEmployee.getText().equalsIgnoreCase("S");
+      boolean employee = tfStudentOrEmployee.getText().equalsIgnoreCase("E");
+      isStudentObject(student);
+
+      
+      if (student) {
+        // if Student object, declare people[index] as Student
+        declarePerson();
+        // Set the header for the next page
+        setHeader(tContinue, tStudent, blank);
+        // Add StudentPane to the pane
+        pane.setCenter(sPane);
+        // Update the stage dimensions
+        primaryStage.setWidth(575);
+        primaryStage.setHeight(310);
+        // Change the button to btNext[1]
+        setButton(1);
+      } else if (employee) {
+        // if Employee object, declare people[index] as Employee
+        declarePerson();
+        // Set the header for the next page
+        setHeader(tContinue, tEmployee, blank);
+        // Add page 1 of the EmployeePane to the pane
+        pane.setCenter(ePane);
+        ePane.page1();
+        // Update stage dimensions
+        primaryStage.setWidth(510);
+        primaryStage.setHeight(270);
+        // Change the button to btNext[2]
+        setButton(2);
      }
-
+     // Clear the TextField
      tfStudentOrEmployee.clear();
     });
 
+
+    // Assign action to btNext[1]
     btNext[1].setOnAction(e -> {
-        
+      
+      // Update variables for this Student object
       individual.setName(sPane.tfName.getText());
       individual.setSocial(sPane.tfSSN.getText());
       individual.setAddress(sPane.tfAddress.getText());
@@ -112,22 +147,22 @@ private int index;
       ((Student) individual).setMajor(sPane.tfMajor.getText());
 
       if (index < people.length) {
-        tRecord.setText(String.format("Record %d", index + 1));
-        tContinue.setText(String.format("Record %d (continued)", index + 1));
-        setHeader(tRecord, blank, new Text(""));
-        setButton(0);
-        firstEntry();
-        primaryStage.setWidth(400);
-        primaryStage.setHeight(160);
+        // If there are more record to add, proceed to the first page
+        firstEntry(primaryStage);
       } else {
+        // Else, close the stage and print all info
         primaryStage.close();
         printInfo();
       }
+      // Clear all sPane text fields 
       sPane.clearTextFields();
     });
 
+
+    // Assign action to btNext[2]
     btNext[2].setOnAction(e -> {
 
+      // Update specified variables for this Employee
       individual.setName(ePane.tfName.getText());
       individual.setSocial(ePane.tfSSN.getText());
       individual.setAddress(ePane.tfAddress.getText());
@@ -135,27 +170,39 @@ private int index;
       individual.setAge(Integer.parseInt(ePane.tfAge.getText()));
       individual.setGender(ePane.tfSex.getText());
 
+      // Remove all Employee nodes
       ePane.removePage1();
+      // Add all nodes for the next page
       ePane.page2();
+      // Adjust width if the stage
       primaryStage.setWidth(525);
+      // Change button to btNext[3]
       setButton(3);
     });
 
+
+    // Assign action to button btNext[3]
     btNext[3].setOnAction(e -> {
       
+      // Update specified variables for this Employee
       ((Employee) individual).setDepartment(ePane.tfDepartment.getText());
       ((Employee) individual).setTitle(ePane.tfTitle.getText());
       ((Employee) individual).setSalaried(ePane.tfHourlyOrSalary.getText());
       ((Employee) individual).setYearHired(ePane.tfYearHired.getText());
 
+      // Determine if Employee is hourly or salary
       boolean hourly = ePane.tfHourlyOrSalary.getText().equalsIgnoreCase("H");
       boolean salary = ePane.tfHourlyOrSalary.getText().equalsIgnoreCase("S");
       ePane.removePage2();
+
+      // if hourly, set up page with hourly questions
       if (hourly) {
         ePane.page3();
         primaryStage.setWidth(690);
         primaryStage.setHeight(200);
         setButton(4);
+
+      // else set up salary questions page
       } else if (salary) {
         primaryStage.setWidth(300);
         primaryStage.setHeight(200);
@@ -164,49 +211,56 @@ private int index;
       }
     });
 
+    
+    // Assign action to button btNext[4]
     btNext[4].setOnAction(e -> {
       
+      // Update specified variabel for this Employee
       ((Employee) individual).setHourlyRate(Float.parseFloat(ePane.tfHourlyRate.getText()));
       ((Employee) individual).setHoursWorked(Float.parseFloat(ePane.tfHoursWorked.getText()));
       ((Employee) individual).setUnionDues(Float.parseFloat(ePane.tfUnionDues.getText()));
 
+      // Remove Employee page 3 nodes
       ePane.removePage3();
+      
+      // if there are more records to add, proceed to the first page of the next record
       if (index < people.length) {
-        primaryStage.setWidth(400);
-        primaryStage.setHeight(160);
-        tRecord.setText(String.format("Record %d", index + 1));
-        tContinue.setText(String.format("Record %d (continued)", index + 1));
-        setHeader(tRecord, blank, new Text(""));
-        setButton(0);
-        firstEntry();
+        firstEntry(primaryStage);
+
+      // else close the window and print all record info
       } else {
         primaryStage.close();
         printInfo();
       }
+
+      // Clear all EmployeePane textfields
       ePane.clearTextFields();
     });
 
+
+    // Assign action to button btNext[5]
     btNext[5].setOnAction(e -> {
 
+      // Update the AnnualSalary for this Employee
       ((Employee) individual).setAnnualSalary(ePane.tfAnnualSalary.getText());
-      
+      // Remove employee page 4
       ePane.removePage4();
+
+      // if there are more records to add, proceed to the first page fo the next record
       if (index < people.length) {
-        primaryStage.setWidth(400);
-        primaryStage.setHeight(160);
-        tRecord.setText(String.format("Record %d", index + 1));
-        tContinue.setText(String.format("Record %d (continued)", index + 1));
-        setHeader(tRecord, blank, new Text(""));
-        setButton(0);
-        firstEntry();
+        firstEntry(primaryStage);
+      // else close the window and print all record info
       } else {
         primaryStage.close();
         printInfo();
       }
+      // Clear all EmployeePane text fields
       ePane.clearTextFields();
     });
   }
 
+
+  /** Prints the toString() info for each people[i]. */
   private void printInfo() {
     for (int i = 0; i < people.length; i++) {
       System.out.println();
@@ -215,20 +269,32 @@ private int index;
     }
   }
 
+
+  /** Assigns isStudent to True/False if answer is True/False.
+      @param answer True or False
+    */
   private void isStudentObject(boolean answer) {
     isStudent = answer;
   }
 
+
+  /** Position the btNext[index] in the buttom right of the pane.
+      @param index The index of btNext
+    */
   private void setButton(int index) {
     pane.setBottom(btNext[index]);
     pane.setAlignment(btNext[index], Pos.CENTER_RIGHT);
   }
 
+
+  /** Set up each btNext[i]. */
   private void declareButtons() {
     for (int i = 0; i < btNext.length; i++)
       btNext[i] = new Button("NEXT");
   }
+  
 
+  /** Declares people[index] as the appropriate object (Student or Employee). */
   private void declarePerson() {
     if (isStudent) {
       people[index] = new Student();
@@ -240,22 +306,47 @@ private int index;
       index++;
     }
   }
+  
 
+  /** Sets the header.
+      @param top The text at the top
+      @param middle The text in the middle
+      @param bottom The text at the bottom
+    */
   private void setHeader(Text top, Text middle, Text bottom) {
     VBox vbox = new VBox(5);
     vbox.getChildren().addAll(top, middle, bottom);
     pane.setTop(vbox);
   }
+  
 
-  private void firstEntry() {
+  /** Set up the first page the current record.
+      @param stage The primaryStage
+    */
+  private void firstEntry(Stage stage) {
+    // Update width and height of the stage
+    stage.setWidth(400);
+    stage.setHeight(160);
+
+    // Update the title texts
+    tRecord.setText(String.format("Record %d", index + 1));
+    tContinue.setText(String.format("Record %d (continued)", index + 1));
+
+    // Set the header and the NEXT button
+    setHeader(tRecord, blank, new Text(""));
+    setButton(0);
+    
+    // Add the Student/Emplyee question
     HBox hbox = new HBox(5);
     hbox.getChildren().addAll(new Label("Student or Employee (S/E):"), tfStudentOrEmployee);
     hbox.setAlignment(Pos.CENTER);
     pane.setCenter(hbox);
   }
 
+
+  /** Pane for displaying Student forms. */
   private class StudentPane extends GridPane {
-   
+    // TextFields
     private TextField tfType = new TextField();
     private TextField tfName = new TextField();
     private TextField tfMajor = new TextField();
@@ -267,6 +358,7 @@ private int index;
     private TextField tfGPA = new TextField();
     private TextField tfYearOfGrad = new TextField();
 
+    /** Constructs a StudentPane object. */
     private StudentPane() {
       setPadding(new Insets(5, 5, 5, 5));
       setHgap(5);
@@ -276,7 +368,8 @@ private int index;
       tfAge.setPrefColumnCount(2);
       tfGPA.setPrefColumnCount(2);
       tfYearOfGrad.setPrefColumnCount(3);
-
+      
+      // Add all column 1 nodes to this StudentPane
       add(new Label("Name:"), 0, 0);
       add(tfName, 1, 0);
       add(new Label("Major:"), 0, 1);
@@ -287,7 +380,7 @@ private int index;
       add(tfAddress, 1, 3);
       add(new Label("Phone (xxx-xxxx):"), 0, 4);
       add(tfPhone, 1, 4);
-
+      // Add all column 2 nodes to this StudentPane
       add(new Label("\tAge:"), 2, 0);
       add(tfAge, 3, 0);
       add(new Label("\tGender (M/F/O):"), 2, 1);
@@ -297,7 +390,8 @@ private int index;
       add(new Label("\tGraduation Year (YYYY):"), 2, 3);
       add(tfYearOfGrad, 3, 3);
     }
-
+    
+    /** Clear all TextFields for this StudentPane. */
     private void clearTextFields() {
       tfName.clear();
       tfMajor.clear();
@@ -312,8 +406,10 @@ private int index;
     }
   }
 
-  private class EmployeePane extends GridPane {
 
+  /** Pane for displaying Employee forms. */
+  private class EmployeePane extends GridPane {
+    // TextFields
     private TextField tfName = new TextField();
     private TextField tfMajor = new TextField();
     private TextField tfSSN = new TextField();
@@ -330,6 +426,7 @@ private int index;
     private TextField tfUnionDues = new TextField();
     private TextField tfAnnualSalary = new TextField();
 
+    // Labels
     private Label lName = new Label("Name:");
     private Label lSSN = new Label("SSN (xxx-xx-xxxx):");
     private Label lAddress = new Label("Address:");
@@ -344,7 +441,8 @@ private int index;
     private Label lHoursWorked = new Label("\tHours Worked (xx.xx):");
     private Label lUnionDues = new Label("\tUnion Dues (xx.xx)");
     private Label lAnnualSalary = new Label("Annual Salary:");
-
+    
+    /** Constructs an EmployeePane object. */
     private EmployeePane() {
       setPadding(new Insets(5, 5, 5, 5));
       setHgap(5);
@@ -359,7 +457,9 @@ private int index;
       tfUnionDues.setPrefColumnCount(4);
       tfAnnualSalary.setPrefColumnCount(6);      
     }
+    
 
+    /** Clears all TextFields for this EmployeePane. */
     private void clearTextFields() {
       tfName.clear();
       tfSSN.clear();
@@ -377,6 +477,8 @@ private int index;
       tfAnnualSalary.clear();
     }
 
+    
+    /** Adds nodes for the first Employee page. */
     private void page1() {
       add(lName, 0, 0);
       add(tfName, 1, 0);
@@ -393,6 +495,8 @@ private int index;
       add(tfSex, 3, 1);
     }
 
+
+    /** Adds nodes for the second Employee page. */
     private void page2() {
       add(lDepartment, 0, 0);
       add(tfDepartment, 1, 0);
@@ -405,6 +509,8 @@ private int index;
       add(tfYearHired, 3, 1);
     }
 
+
+    /** Adds nodes for the third Employee page. */
     private void page3() {
       add(lHourlyRate, 0, 0);
       add(tfHourlyRate, 1, 0);
@@ -416,11 +522,15 @@ private int index;
       add(tfUnionDues, 5, 0);
     }
 
+
+    /** Adds nodes for the fourth Employee page. */
     private void page4() {
       add(lAnnualSalary, 1, 0);
       add(tfAnnualSalary, 2, 0);
     }
 
+
+    /** Removes all nodes from the first Employee page. */
     private void removePage1() {
       getChildren().remove(lName);
       getChildren().remove(lSSN);
@@ -437,6 +547,8 @@ private int index;
       getChildren().remove(tfSex);
     }
 
+
+    /** Removes all nodes from the decond Employee page. */
     private void removePage2() {
       getChildren().remove(lDepartment);
       getChildren().remove(lTitle);
@@ -449,8 +561,9 @@ private int index;
       getChildren().remove(tfYearHired);
     }
 
-    private void removePage3() {
-      
+
+    /** Removes all nodes from the third Employee page. */
+    private void removePage3() {      
       getChildren().remove(lHourlyRate);
       getChildren().remove(tfHourlyRate);
 
@@ -461,11 +574,12 @@ private int index;
       getChildren().remove(tfUnionDues);
     }
 
+
+    /** Removes all nodes from the fourth Employee page. */
     private void removePage4() {
       getChildren().remove(lAnnualSalary);
       getChildren().remove(tfAnnualSalary);
     }
-  
   }
 
   /** The main method. */
