c9a67a282ef2b60908afeb70cc7520adba66800d
Mr. Young <masyoung@hawaii.edu>
2020-08-14 00:29:54 -1000

    Completed Exercises 2.3.1 and 2.3.2 ...while drinking :-)

--- Chapter_2/Exercise01_01.java
+++ Chapter_2/Exercise01_01.java
@@ -3,15 +3,12 @@ public class Exercise01_01 {
   /** The main method. */
   public static void main(String[] args) {
    
-    int y1 = 0;
-    int y2 = 0;
-
     System.out.println();
     System.out.println(" n  |   y1      y2");
     System.out.println("-------------------");
     for (int n = 0; n <= 100; n += 10) {
-      y1 = (100 * n) + 10;
-      y2 = (5 * (n * n)) + 2;
+      int y1 = (100 * n) + 10;
+      int y2 = (5 * (n * n)) + 2;
       System.out.printf("%3d |%6d %6d\n",n, y1, y2);
       System.out.println("-------------------");
     }
